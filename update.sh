a2df9395aee08f7a4163c498e4afe9ea1eb2e863
Armin Gorji <arvage@yahoo.com>
2020-09-16 00:30:54 -0700

    fixed coloring

--- update.sh
+++ update.sh
@@ -11,8 +11,8 @@ GREEN='\033[0;32m'      # Green
 if [[ "${UID}" -ne 0 ]]
 then 
   echo;echo
-  echo "${RED}You must be root to run this command." >&2
-  echo "Please run it with sudo${NC}" >&2
+  echo -e "${RED}You must be root to run this command." >&2
+  echo -e "Please run it with sudo${NC}" >&2
   echo
   exit 1
 fi
